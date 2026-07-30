921d0e0cd7f550d3d381602967e2a947cc8b4384
Gerhard Pisch <github@gecomp.at>
2026-07-31 00:10:18 +0200

    GitHub Copilot [LLM: kimi-k2.7-code:cloud]: Entferne CompatibilityLevel-Prüfung aus FileContent Lifecycle-Contract
    - Lifecycle-Contract benötigt kein CompatibilityLevel-Argument
    
    - Verhindert sqlcmd-Fehler 'CompatibilityLevel scripting variable not defined'

--- Modules/toolbelt.file.content/Tests/Runtime/Lifecycle.Contract.sql
+++ Modules/toolbelt.file.content/Tests/Runtime/Lifecycle.Contract.sql
@@ -1,12 +1,5 @@
 SET NOCOUNT ON;
 
-DECLARE @CompatibilityLevel int = TRY_CONVERT(int, N'$(CompatibilityLevel)');
-IF @CompatibilityLevel NOT IN (150, 160, 170)
-    THROW 52930, N'CompatibilityLevel muss 150, 160 oder 170 sein.', 1;
-IF (SELECT compatibility_level FROM sys.databases WHERE database_id = DB_ID())
-       <> @CompatibilityLevel
-    THROW 52931, N'Falscher Compatibility Level.', 1;
-
 IF OBJECT_ID(N'toolbelt_file.USP_LoadBinaryFile', N'P') IS NULL
     THROW 52932, N'USP_LoadBinaryFile fehlt oder besitzt den falschen Typ.', 1;
 IF OBJECT_ID(N'toolbelt_file.USP_LoadTextFile', N'P') IS NULL
