f31732a05b65401edb4dc0e6fce7b4e7e4c9548a
Gerhard Pisch <github@gecomp.at>
2026-07-30 22:09:27 +0200

    GitHub Copilot [LLM: kimi-k2.6:cloud]: Fix little-endian binary integer parsing in USP_ExtractZipEntryFromBinary

--- Modules/toolbelt.archive.zip-memory/Source/USP_ExtractZipEntryFromBinary.sql
+++ Modules/toolbelt.archive.zip-memory/Source/USP_ExtractZipEntryFromBinary.sql
@@ -215,13 +215,13 @@ EXEC toolbelt_archive.USP_ExtractZipEntryFromBinary
         THROW 51321, N'EOCD-Signatur wurde im ZIP-Container nicht gefunden.', 1;
 
     SET @TotalEntries =
-        CONVERT(int, CONVERT(varbinary(2), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 10), 2))));
+        CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 11), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 10), 1));
     SET @CentralDirectorySize =
-        CONVERT(int, CONVERT(varbinary(4), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 12), 4))));
+        CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 15), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 14), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 13), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 12), 1));
     SET @CentralDirectoryOffset =
-        CONVERT(int, CONVERT(varbinary(4), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 16), 4))));
+        CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 19), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 18), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 17), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 16), 1));
     SET @CommentLength =
-        CONVERT(int, CONVERT(varbinary(2), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 20), 2))));
+        CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 21), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @EocdStartPos + 20), 1));
 
     IF @TotalEntries < 1
         THROW 51321, N'Das ZIP-Archiv enthaelt keine Entries.', 1;
@@ -273,23 +273,23 @@ EXEC toolbelt_archive.USP_ExtractZipEntryFromBinary
             , @HeaderLength int;
 
         SET @GpFlags =
-            CONVERT(int, CONVERT(varbinary(2), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 8), 2))));
+            CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 9), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 8), 1));
         SET @Method =
-            CONVERT(int, CONVERT(varbinary(2), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 10), 2))));
+            CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 11), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 10), 1));
         SET @Crc32 =
-            CONVERT(int, CONVERT(varbinary(4), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 16), 4))));
+            CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 19), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 18), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 17), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 16), 1));
         SET @CompressedSize =
-            CONVERT(int, CONVERT(varbinary(4), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 20), 4))));
+            CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 23), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 22), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 21), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 20), 1));
         SET @UncompressedSize =
-            CONVERT(int, CONVERT(varbinary(4), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 24), 4))));
+            CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 27), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 26), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 25), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 24), 1));
         SET @NameLength =
-            CONVERT(int, CONVERT(varbinary(2), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 28), 2))));
+            CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 29), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 28), 1));
         SET @ExtraLength =
-            CONVERT(int, CONVERT(varbinary(2), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 30), 2))));
+            CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 31), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 30), 1));
         SET @CommentLen =
-            CONVERT(int, CONVERT(varbinary(2), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 32), 2))));
+            CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 33), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 32), 1));
         SET @LocalOffset =
-            CONVERT(int, CONVERT(varbinary(4), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 42), 4))));
+            CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 45), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 44), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 43), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @CursorPos + 42), 1));
 
         IF @NameLength < 1 OR @NameLength > 1024 OR @ExtraLength < 0 OR @CommentLen < 0
             THROW 51321, N'Entry-Metadaten sind ausserhalb der unterstuetzten Grenzen.', 1;
@@ -396,19 +396,19 @@ EXEC toolbelt_archive.USP_ExtractZipEntryFromBinary
         THROW 51321, N'Der Local Header besitzt eine ungueltige Signatur.', 1;
 
     SET @LocalFlags =
-        CONVERT(int, CONVERT(varbinary(2), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 6), 2))));
+        CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 7), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 6), 1));
     SET @LocalMethod =
-        CONVERT(int, CONVERT(varbinary(2), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 8), 2))));
+        CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 9), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 8), 1));
     SET @LocalCrc =
-        CONVERT(int, CONVERT(varbinary(4), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 14), 4))));
+        CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 17), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 16), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 15), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 14), 1));
     SET @LocalCompressed =
-        CONVERT(int, CONVERT(varbinary(4), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 18), 4))));
+        CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 21), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 20), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 19), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 18), 1));
     SET @LocalUncompressed =
-        CONVERT(int, CONVERT(varbinary(4), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 22), 4))));
+        CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 25), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 24), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 23), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 22), 1));
     SET @LocalNameLen =
-        CONVERT(int, CONVERT(varbinary(2), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 26), 2))));
+        CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 27), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 26), 1));
     SET @LocalExtraLen =
-        CONVERT(int, CONVERT(varbinary(2), REVERSE(SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 28), 2))));
+        CONVERT(int, SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 29), 1) + SUBSTRING(@ZipArchive, CONVERT(int, @LocalHeaderPos + 28), 1));
 
     IF @LocalNameLen < 1 OR @LocalNameLen > 1024 OR @LocalExtraLen < 0
         THROW 51321, N'Der Local Header enthaelt ungueltige Feldlaengen.', 1;
